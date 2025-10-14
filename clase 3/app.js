b4e65d964bf496a3df6b83d4df7f84d297e6b6a9
DevelopingJeremy <quesadajeremy7@gmail.com>
2025-10-13 18:33:43 -0600

    Cambio de computadora

--- clase 3/app.js
+++ clase 3/app.js
@@ -1,19 +1,62 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 0;
-const http = require('http');
+const PORT = process.env.PORT || 3000;
+const movies = require('./movies.json');
 
-// Middlewares Basicos
 app.use(express.json());
 app.disable('x-powered-by');
 
-
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hola, mundo!\n');
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
 });
 
-server.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${server.address().port}`);
+app.get('/movies', (req, res) => {
+    const { genre } = req.query;
+
+    if (genre) {
+        const moviesFiltered = movies.filter(
+            m => m.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        );
+        return res.status(200).json(moviesFiltered);
+    }
+
+    res.status(200).json(movies);
+});
+
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params;
+    const movie = movies.find(m => m.id === Number(id));
+
+    if (movie) res.status(200).json(movie);
+    else res.status(404).json({ error: 'Pelicula no encontrada' });
+});
+
+app.post('/movies', (req, res) => {
+    const {
+        title,
+        year,
+        director,
+        duration,
+        genre,
+        rate
+    } = req.body;
+
+    const newMovie = {
+        id: crypto.randomUUID(),
+        title,
+        year,
+        director,
+        duration,
+        genre,
+        rate: rate ?? 0
+    };
+
+    // Esto no es REST, porque estamos modificando un recurso en memoria
+    movies.push(newMovie);
+
+    res.status(201).json(newMovie);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
---
+++ clase 3/schemas/movies.js
@@ -0,0 +1,21 @@
+const z = require('zod');
+
+const movieSchema = z.object({
+    title: z.string({
+        required_error: 'El título es obligatorio',
+        invalid_type_error: 'El título debe ser una cadena de texto'
+    }),
+    year: z.number().int().min(1888).max(new Date().getFullYear()),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    genre: z.array(z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Romance', 'Sci-Fi', 'Crime', 'Romance'])).min(1),
+    rate: z.number().min(0).max(10)
+});
+
+function validateMovie(object) {
+    return movieSchema.safeParse(object);
+}
+
+module.exports = {
+    validateMovie
+};
