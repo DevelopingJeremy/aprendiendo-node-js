990f4d8e87498238d3f3888f05171ca5917cb77b
DevelopingJeremy <quesadajeremy7@gmail.com>
2025-11-08 14:21:11 -0600

    add clase 7

--- clase 7/index.js
+++ clase 7/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import jwt from 'jswonwebtoken';
+import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv/config';
 import { UserRepository } from './user-repository.js';
@@ -11,9 +11,27 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+    const token = req.cookies.jwt_token;
+
+    req.session = {
+        user: null
+    }
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY)
+        req.session.user = decoded;
+    } catch (error) {}
+
+        next();
+
+})
+
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const { user } = req.session
+    res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -23,8 +41,18 @@ app.post('/login', async (req, res) => {
         const user = await UserRepository.login({ username, password })
 
         // El jwt tiene (datos, secret key, opciones)
-        const token = jwt.sign({ id: user._id, username: user.username}, SECRET_KEY, { expiresIn: '1h' })
-        res.status(200).json({ user, token })
+        const token = jwt.sign({ id: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' })
+        res
+            .cookie('jwt_token', token,
+                {
+                    httpOnly: true, // La cookie no es accesible desde JavaScript solo desde el server
+                    secure: false, // Solo se envia la cookie por HTTPS
+                    sameSite: 'strict', // Solo se accede desde el mismo dominio
+                    maxAge: 3600000 // 1 hora
+                }
+            )
+            .status(200)
+            .send({ user, token })
     } catch (error) {
         res.status(400).send({ error: error.message })
     }
@@ -40,10 +68,15 @@ app.post('/register', async (req, res) => {
         res.status(400).send({ error: error.message });
     }
 })
-app.post('/logout', (req, res) => {})
+
+app.post('/logout', (req, res) => {
+    res
+    .clearCookie('jwt_token')
+    .json({ message: "Logout succesful"})
+})
 
 app.get('/protected', (req, res) => {
-    res.render('protected', { name: "Jeremito"})
+    res.render('protected', { name: req.session.user.username })
 })
 
 
